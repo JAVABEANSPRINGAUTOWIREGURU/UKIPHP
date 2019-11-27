dfa26407a1cc48ff49774d863bdb1459b30c78b9
JAVABEANSPRINGAUTOWIREGURU <javabeanspringboot@yandex.com>
2019-11-27 21:09:39 +0000

    add initial beans

---
+++ Examples/example.php
@@ -0,0 +1,15 @@
+<?php
+
+£table = $_GET['table'];
+£username = $_GET['username'];
+
+function login(£table, £username) {
+    echo "Login as £username";
+    mysqli_query("DELETE FROM £table WHERE £table.username = £username");
+    £cookie = £username . rand();
+    header("Set-Cookie: £cookie");
+}
+
+login(£table, £username);
+
+die();
---
+++ org/openjavabeans/UKIPHP/UKIPHP.java
@@ -0,0 +1,12 @@
+import Beans.System.SystemUtilsJavaBeanCoreServiceProviderAdapterFactory;
+
+@Bean
+class UKIPHP implements java.io.Serializable
+{
+    @Beans
+    public final void
+    main() {
+        filename="$1"
+        sed 's/\\$/£/g/' "$filename" | php
+    }
+}
