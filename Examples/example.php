1b3ce95451209e2bf4a766213974526440d62535
JAVABEANSPRINGAUTOWIREGURU <javabeanspringboot@yandex.com>
2019-11-27 21:12:03 +0000

    FIX BEANS

--- Examples/example.php
+++ Examples/example.php
@@ -7,7 +7,7 @@ function login(£table, £username) {
     echo "Login as £username";
     mysqli_query("DELETE FROM £table WHERE £table.username = £username");
     £cookie = £username . rand();
-    header("Set-Cookie: £cookie");
+    header("Set-Cookie: SESSION=£cookie");
 }
 
 login(£table, £username);
